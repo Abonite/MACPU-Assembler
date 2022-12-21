7b72cf89f6274c287e6218f78a8bfd134596b273
Abonite <378751910@qq.com>
2022-12-21 20:22:32 +0800

    In the development of precompiled parser

--- src/DIProcessor.rs
+++ src/DIProcessor.rs
@@ -1,3 +1,76 @@
+use std::collections::HashMap;
+use std::num::ParseIntError;
+
+const IDLE: u16                 = 0b0;
+const GET_ARG_1_FIRST_CHAR: u16 = 0b1;
+const GET_ARG_1: u16            = 0b10;
+const ARG_1_FINISH: u16         = 0b100;
+const GET_ARG_2: u16            = 0b1000;
+const FINISH: u16               = 0b10000;
+
+enum ParseError {
+    ArgumentStartWithNumberError(ASWNError),
+    ArgumentStartWithNonUnderlineError(ASWNUError),
+    TooManyArgmumentError(TMAError),
+    StateMachineError(SMError),
+    ParseNumberError(ParseIntError)
+}
+
+#[derive(Debug)]
+struct ASWNError;
+
+impl std::fmt::Display for ASWNError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "First argument can not start with number")
+    }
+}
+
+impl std::error::Error for ASWNError {}
+
+#[derive(Debug)]
+struct ASWNUError;
+
+impl std::fmt::Display for ASWNUError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Argument can not start with a symbol other than '_'")
+    }
+}
+
+impl std::error::Error for ASWNUError {}
+
+#[derive(Debug)]
+struct TMAError;
+
+impl std::fmt::Display for TMAError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Only need one argmument")
+    }
+}
+
+impl std::error::Error for TMAError {}
+
+#[derive(Debug)]
+struct SMError;
+
+impl std::fmt::Display for SMError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "State machine error")
+    }
+}
+
+impl std::error::Error for SMError {}
+
+#[derive(Debug)]
+struct PNError;
+
+impl std::fmt::Display for PNError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Illegal character while parsing number")
+    }
+}
+
+impl std::error::Error for PNError {}
+
 /// The Dot Instrction e.g. ProProcess Instrction in this assemblyer must abide by this syntax format:
 /// .INSTNAME[SPACE]VARIABLENAME[SPACE]VALUE[SPACE or TAB][\n]
 /// or
@@ -14,14 +87,15 @@
 /// and check
 /// 4. Throw Error if it does not conform to the syntax
 
-const IDLE: u16 = 0b0;
-const GET_ARG_FIRST_CHAR: u16 = 0b1;
-const GET_ARG: u16 = 0b10;
-const FINISH: u16 = 0b100;
-
-
 pub struct DotInstrctionsProcessor {
     dot_instrctions: Vec<(usize, String)>,
+    set_info: HashMap<String, setting_items>
+}
+
+enum setting_items {
+    sibool(bool),
+    sinum(usize),
+    sistr(String)
 }
 
 impl DotInstrctionsProcessor {
@@ -39,13 +113,40 @@ impl DotInstrctionsProcessor {
             }
         }
         (DotInstrctionsProcessor {
-            dot_instrctions
+            dot_instrctions,
+            set_info: HashMap::new()
         }, instrcutions)
     }
 
-    pub fn generate(&self) {
+    pub fn generate(&mut self) {
         for (line_num, line) in self.dot_instrctions.clone() {
             if line.starts_with(".SET") {
+                match self.two_args(line.trim_start_matches(".SET")) {
+                    Ok((si, sv)) => {
+                        if self.set_info.contains_key(&si) {
+                            self.set_info.insert(si, sv);
+                        }
+                    },
+                    Err(pe) => {
+                        match pe {
+                            ParseError::ArgumentStartWithNonUnderlineError(e) => {
+
+                            },
+                            ParseError::ArgumentStartWithNumberError(e) => {
+
+                            },
+                            ParseError::ParseNumberError(e) => {
+
+                            },
+                            ParseError::StateMachineError(e) => {
+
+                            },
+                            ParseError::TooManyArgmumentError(e) => {
+
+                            }
+                        }
+                    }
+                }
 
             } else if line.starts_with(".DATA") {
 
@@ -59,9 +160,8 @@ impl DotInstrctionsProcessor {
         }
     }
 
-    fn one_arg(&self, line: String) {
-        /// line must be removed start
-
+    /// Instructions that need to remove the beginning of a line
+    fn one_arg(&self, line: &str) -> Result<String, ParseError> {
         let mut curr_state = IDLE;
 
         let mut arg = String::new();
@@ -69,37 +169,149 @@ impl DotInstrctionsProcessor {
         for c in line.chars() {
             match curr_state {
                 IDLE => {
-                    if c == ' ' {
-                        curr_state = GET_ARG_FIRST_CHAR;
+                    if c == ' ' && c == '\t' {
+                        curr_state = GET_ARG_1_FIRST_CHAR;
                     }
                 },
                 GET_ARG_FIRST_CHAR => {
                     if c.is_ascii_digit() {
-                        // error: start with digit
+                        return Err(ParseError::ArgumentStartWithNumberError(ASWNError));
                     } else if c.is_ascii_punctuation() && c != '_' {
-                        // error: can not start with punctuation but "_"
+                        return Err(ParseError::ArgumentStartWithNonUnderlineError(ASWNUError));
                     } else {
                         arg.push(c);
-                        curr_state = GET_ARG;
+                        curr_state = GET_ARG_1;
                     }
                 },
                 GET_ARG => {
-                    if c != ' ' {
+                    if c != '\n' && c != ' ' && c != '\t' {
                         arg.push(c)
                     } else {
-                        curr_state = FINISH;
+                        curr_state = ARG_1_FINISH;
                     }
                 },
-                FINISH => {
-                    if c != ' ' || c != '\t' {
-                        // error: too many args
+                ARG_1_FINISH => {
+                    if c != ' ' && c != '\t' && c != '\n' {
+                        return Err(ParseError::TooManyArgmumentError(TMAError));
                     }
                 },
                 _ => {
-                    curr_state == IDLE;
-                    // error: state macheine error
+                    curr_state = IDLE;
+                    return Err(ParseError::StateMachineError(SMError));
                 }
             }
+        }
+        return Ok(arg);
+    }
+
+    /// Instructions that need to remove the beginning of a line
+    fn two_args(&self, line: &str) -> Result<(String, setting_items), ParseError> {
+        let arg_str: u8 = 0b0;
+        let arg_not_str: u8 = 0b1;
+
+        let mut curr_state = IDLE;
+
+        let mut arg_2_type = arg_not_str;
+
+        let mut arg_1 = String::new();
+        let mut arg_2 = String::new();
+
+        for c in line.chars() {
+            match curr_state {
+                IDLE => {
+                    if c == ' ' && c == '\t' {
+                        curr_state = GET_ARG_1_FIRST_CHAR;
+                    }
+                },
+                GET_ARG_1_FIRST_CHAR => {
+                    if c.is_ascii_digit() {
+                        return Err(ParseError::ArgumentStartWithNumberError(ASWNError));
+                    } else if c.is_ascii_punctuation() && c != '_' {
+                        return Err(ParseError::ArgumentStartWithNonUnderlineError(ASWNUError));
+                    } else {
+                        arg_1.push(c);
+                        curr_state = GET_ARG_1;
+                    }
+                },
+                GET_ARG_1 => {
+                    if c != ' ' && c != '\t' {
+                        arg_1.push(c)
+                    } else {
+                        curr_state = ARG_1_FINISH;
+                    }
+                },
+                ARG_1_FINISH => {
+                    if c != ' ' && c != '\t' {
+                        if c == '\"' {
+                            arg_2_type = arg_str;
+                            curr_state = GET_ARG_2;
+                        } else {
+                            arg_2.push(c);
+                            curr_state = GET_ARG_2;
+                        }
+                    }
+                },
+                GET_ARG_2 => {
+                    if arg_2_type == arg_not_str {
+                        if c != ' ' && c != '\t' && c != '\n' {
+                            arg_2.push(c);
+                        } else {
+                            curr_state = FINISH;
+                        }
+                    } else if arg_2_type == arg_str {
+                        if c != '\"' {
+                            arg_2.push(c);
+                        } else {
+                            curr_state = FINISH;
+                        }
+                    } else {
+                        return Err(ParseError::StateMachineError(SMError));
+                    }
+                },
+                FINISH => {
+                    if c != ' ' && c != '\t' && c != '\n' {
+                        return Err(ParseError::TooManyArgmumentError(TMAError));
+                    }
+                }
+                _ => {
+                    curr_state = IDLE;
+                    return Err(ParseError::StateMachineError(SMError));
+                }
+            }
+        }
+
+        if arg_2_type == arg_not_str {
+            if arg_2.ends_with("H") {
+                match usize::from_str_radix(arg_2.trim_end_matches("H"), 16) {
+                    Ok(a) => return Ok((arg_1, setting_items::sinum(a))),
+                    Err(e) => return Err(ParseError::ParseNumberError(e))
+                };
+            } else if arg_2.ends_with("O") {
+                match usize::from_str_radix(arg_2.trim_end_matches("O"), 8) {
+                    Ok(a) => return Ok((arg_1, setting_items::sinum(a))),
+                    Err(e) => return Err(ParseError::ParseNumberError(e))
+                };
+            } else if arg_2.ends_with("B") {
+                match usize::from_str_radix(arg_2.trim_end_matches("B"), 2) {
+                    Ok(a) => return Ok((arg_1, setting_items::sinum(a))),
+                    Err(e) => return Err(ParseError::ParseNumberError(e))
+                };
+            } else {
+                match usize::from_str_radix(arg_2.as_str(), 10) {
+                    Ok(a) => return Ok((arg_1, setting_items::sinum(a))),
+                    Err(e) => return Err(ParseError::ParseNumberError(e))
+                };
+            }
+        } else if arg_2_type == arg_str {
+            if arg_2.to_ascii_uppercase() == "TRUE" {
+                return Ok((arg_1, setting_items::sibool(true)));
+            } else if arg_2.to_ascii_uppercase() == "FALSE" {
+                return Ok((arg_1, setting_items::sibool(false)));
+            } else {
+                return Ok((arg_1, setting_items::sistr(arg_2)));
+            }
+        } else {
+            return Err(ParseError::StateMachineError(SMError));
         }
     }
 }
--- src/SFSpliter.rs
+++ src/SFSpliter.rs
@@ -5,6 +5,18 @@ pub fn SourceFileSpliter(assembly_file_path: &str) -> Vec<(usize, String)> {
     let mut sf_data = String::new();
     sf.read_to_string(&mut sf_data).unwrap();
 
-    sf_data.trim().lines().enumerate().map(|x| (x.0, String::from(x.1.trim()))).collect::<Vec<_>>()
-    // TODO: remove comments
+    let sf_data = sf_data.trim().lines().enumerate().map(
+        |(line_num, line)|
+        (line_num, line.trim())
+    ).collect::<Vec<_>>();
+
+    let mut no_comments_data = vec![];
+    for (line_num, line) in sf_data {
+        if line == "" {
+            continue;
+        }
+        no_comments_data.push((line_num, String::from(line.split(";").collect::<Vec<_>>()[0].trim())));
+    }
+
+    no_comments_data
 }
