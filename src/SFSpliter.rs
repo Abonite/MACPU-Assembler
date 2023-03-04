bb035704bca9ace0d0d9f13fb15c8883c50addb8
Abonite <378751910@qq.com>
2023-03-04 22:34:43 +0800

    modified:   README.md 	modified:   src/SFSpliter.rs

--- src/SFSpliter.rs
+++ src/SFSpliter.rs
@@ -5,11 +5,14 @@ pub fn SourceFileSpliter(assembly_file_path: &str) -> Vec<(usize, String)> {
     let mut sf_data = String::new();
     sf.read_to_string(&mut sf_data).unwrap();
 
+    // Record the content and line number of each line of the original file to
+    // facilitate subsequent detection of various errors in the file
     let sf_data = sf_data.trim().lines().enumerate().map(
         |(line_num, line)|
         (line_num, line.trim())
     ).collect::<Vec<_>>();
 
+    // Remove all comment lines and blank lines in the file
     let mut no_comments_data = vec![];
     for (line_num, line) in sf_data {
         if line == "" {
