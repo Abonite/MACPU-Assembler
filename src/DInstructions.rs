e075ec472d14c669b932495cb4e8d1baeb2d2bb5
Abonite <378751910@qq.com>
2023-03-13 22:44:46 +0800

    modified:   src/DInstructions.rs

--- src/DInstructions.rs
+++ src/DInstructions.rs
@@ -17,6 +17,26 @@ impl Display for UnparseableStringError {
     }
 }
 
+#[derive(Debug)]
+struct ValueOutOfExpressionRangeError {
+    value: String,
+    v_type: String
+}
+
+impl Error for ValueOutOfExpressionRangeError {}
+impl Display for ValueOutOfExpressionRangeError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "The value {} used exceeds the limit of {}", self.value, self.v_type)
+    }
+}
+
+enum UnExceptedErrors {
+    USE(UnparseableStringError),
+    VOOERE(ValueOutOfExpressionRangeError),
+    PIE(ParseIntError),
+    JS(String)
+}
+
 // Settings table
 const SETTINGS: HashMap<String, Setting_item> = HashMap::from_iter(vec![
     (String::from("S_CODE_SA"), Setting_item::I(0x0)),
@@ -86,6 +106,101 @@ impl SET {
             "FALSE" => Ok(false),
             "TRUE" => Ok(true),
             _ => return Err(UnparseableStringError{inst: String::from("SET"), value: self.value})
+        }
+    }
+
+    fn Int(&self) -> Result<u32, ParseIntError> {
+        if self.value.starts_with("hex") {
+            return u32::from_str_radix(self.value.trim_start_matches("hex"), 16);
+        } else if self.value.starts_with("oct") {
+            return u32::from_str_radix(self.value.trim_start_matches("oct"), 8);
+        } else if self.value.starts_with("bin") {
+            return u32::from_str_radix(self.value.trim_start_matches("bin"), 2);
+        } else {
+            return u32::from_str_radix(self.value.as_str(), 10);
+        }
+    }
+}
+
+pub struct VAR {
+    name: String,
+    data_type: String,
+    value: String
+}
+
+impl VAR {
+    pub fn new(name: String, data_type: String, value: String) -> VAR {
+        VAR {
+            name,
+            data_type,
+            value
+        }
+    }
+
+    pub fn generateData(&self) -> Result<(u32, u8), String> {
+        let mut error_infos = String::new();
+        let mut error = false;
+    }
+
+    fn calcData(&self) -> Result<(u32, u8), UnExceptedErrors> {
+        // TODO: how to represent negative numbers
+        match self.data_type.as_str() {
+            "byte" | "ubyte" => {
+                match self.Int() {
+                    Ok(v) => {
+                        if v > u8::MAX as u32 {
+                            return Err(UnExceptedErrors::VOOERE(ValueOutOfExpressionRangeError{value: self.value, v_type: String::from("byte or ubyte")}));
+                        } else {
+                            return Ok((v, 1));
+                        }
+                    },
+                    Err(e) => {
+                        return Err(UnExceptedErrors::PIE(e));
+                    }
+                }
+            },
+            "word" | "uword" => {
+                match self.Int() {
+                    Ok(v) => {
+                        if v > u16::MAX as u32 {
+                            return Err(UnExceptedErrors::VOOERE(ValueOutOfExpressionRangeError{value: self.value, v_type: String::from("word or uword")}));
+                        } else {
+                            return Ok((v, 1));
+                        }
+                    },
+                    Err(e) => {
+                        return Err(UnExceptedErrors::PIE(e));
+                    }
+                }
+            },
+            "dword" | "udword" => {
+                match self.Int() {
+                    Ok(v) => {
+                        if v > u8::MAX as u32 {
+                            return Err(UnExceptedErrors::VOOERE(ValueOutOfExpressionRangeError{value: self.value, v_type: String::from("byte")}));
+                        } else {
+                            return Ok((v, 1));
+                        }
+                    },
+                    Err(e) => {
+                        return Err(UnExceptedErrors::PIE(e));
+                    }
+                }
+            },
+            _ => {
+                match self.Int() {
+                    Ok(v) => {
+                        if v > u8::MAX as u32 {
+                            return Err(UnExceptedErrors::VOOERE(ValueOutOfExpressionRangeError{value: self.value, v_type: String::from("byte")}));
+                        } else {
+                            return Ok((v, 1));
+                        }
+                    },
+                    Err(e) => {
+                        return Err(UnExceptedErrors::PIE(e));
+                    }
+                }
+            },
         }
     }
 
